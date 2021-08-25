9da3928ea66fde8e651b1dcb35f833a492f242fe
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-08-24 17:39:45 -0700

    rendering Lobby for debugging

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 // import Home from './pages/home';
-import Fight from './pages/fight';
+// import Fight from './pages/fight';
+import Lobby from './pages/lobby';
 
 export default class App extends React.Component {
   render() {
-    return <Fight />;
+    return <Lobby />;
   }
 }
