54a6e934eec2b02eac2ccfe1802d3ce151fb5c60
Moses Kim <mosesrandykim@gmail.com>
2021-08-25 16:00:18 -0700

    added a background image that takes up 100% width and 100vh. Also the z-index is set to -1

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-// import Home from './pages/home';
-import Fight from './pages/fight';
+import Home from './pages/home';
+// import Fight from './pages/fight';
 
 export default class App extends React.Component {
   render() {
-    return <Fight />;
+    return <Home />;
   }
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
+      <div className="backgroundContainer">
+        <img src="sf2background.png" alt="street fighter 2 background" className="backgroundImage" />
+      </div>
+      <div>
+        <h1>Hello World!</h1>
+      </div>
     </>
   );
 }
