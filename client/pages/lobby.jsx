daa85818208bc846c2e2355969eab57c3bac6a84
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-08-25 14:23:21 -0700

    refactored to use styled

--- client/pages/lobby.jsx
+++ client/pages/lobby.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
-import { Container, Button, List, ListItem, ListItemText, makeStyles } from '@material-ui/core';
+import { Container, Button, List, ListItem, ListItemText, styled } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  createPostButton: {
-    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px'
-  },
-  post: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px'
-  }
+const PostGameButton = styled(Button)({
+  background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px'
+});
+
+const PostedGame = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px'
 });
 
 export default function Lobby(props) {
@@ -41,24 +40,22 @@ export default function Lobby(props) {
     }
   ]);
 
-  const classes = useStyles();
-
   // useEffect(() => {
   //   console.log('fetching');
   // });
 
   return (
     <Container>
-      <Button variant="contained" className={classes.createPostButton}>
+      <PostGameButton variant="contained">
         Post A Game
-      </Button>
+      </PostGameButton>
       <List>
         {posts.map(post => (
           <ListItem key={post.gameId}>
             <ListItemText>
-              <Button variant="contained" className={classes.post}>
+              <PostedGame variant="contained">
                 Join Game {post.gameId}
-              </Button>
+              </PostedGame>
             </ListItemText>
           </ListItem>
         ))}
