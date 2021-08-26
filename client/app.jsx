67779f30d94ade3a791c4e4773711834b15e1edd
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-08-25 17:23:03 -0700

    no longer displaying lobby at start

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 // import Home from './pages/home';
-// import Fight from './pages/fight';
-import Lobby from './pages/lobby';
+import Fight from './pages/fight';
 
 export default class App extends React.Component {
   render() {
-    return <Lobby />;
+    return <Fight />;
   }
 }
