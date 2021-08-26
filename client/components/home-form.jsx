2d9dd9e0410010f4042a9612b139c82cbc209950
Moses Kim <mosesrandykim@gmail.com>
2021-08-25 20:08:08 -0700

    added styling to the form and added a font file

--- client/components/home-form.jsx
+++ client/components/home-form.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function HomeForm(props) {
   return (
-    <form action="">
-      <input type="text" name="username" id="username" placeholder="Username" autoComplete="off" />
-      <button type="submit">Start</button>
+    <form action="" className="homeForm">
+      <input type="text" name="playerName" id="playerName" placeholder="Player Name" autoComplete="off" spellCheck="false"/>
+      <button type="submit" className="startButton">Start</button>
     </form>
   );
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeForm from '../components/home-form';
 import { styled } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 
 const BGContainer = styled('div')(() => ({
   position: 'relative'
@@ -14,16 +15,31 @@ const BGImage = styled('img')(() => ({
   position: 'fixed',
   top: 0,
   left: 0,
-  zIndex: -1
+  zIndex: -2
+}));
+
+const BGOverlay = styled('div')(() => ({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  minWidth: '100%',
+  minHeight: '100vh',
+  zIndex: -1,
+  backgroundColor: 'rgba(0, 0, 0, 0.5)'
 }));
 
 export default function Home(props) {
   return (
     <>
       <BGContainer>
+        <BGOverlay />
         <BGImage src="sf2background.png" alt="street fighter 2 background"/>
       </BGContainer>
-      <HomeForm />
+      <Grid container justifyContent="center" alignContent="center">
+        <Grid item>
+          <HomeForm />
+        </Grid>
+      </Grid>
     </>
   );
 }
