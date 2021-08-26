019032f17cef02ac66298a013ad1165f381b6942
Moses Kim <mosesrandykim@gmail.com>
2021-08-25 21:21:53 -0700

    added portraits to the home page

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import Home from './pages/home';
 // import Fight from './pages/fight';
+import { styled } from '@material-ui/core/styles';
 
-export default class App extends React.Component {
-  render() {
-    return <Home />;
-  }
+const BGContainer = styled('div')(() => ({
+  position: 'relative'
+}));
+
+const BGImage = styled('img')(() => ({
+  display: 'block',
+  minWidth: '100%',
+  maxHeight: '100vh',
+  minHeight: '100vh',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  zIndex: -2
+}));
+
+const BGOverlay = styled('div')(() => ({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  minWidth: '100%',
+  minHeight: '100vh',
+  zIndex: -1,
+  backgroundColor: 'rgba(0, 0, 0, 0.5)'
+}));
+
+export default function App() {
+  return (
+    <>
+      <BGContainer>
+        <BGOverlay />
+        <BGImage src="sf2background.png" alt="street fighter 2 background" />
+      </BGContainer>
+      <Home />
+    </>
+  );
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,40 +1,10 @@
 import React from 'react';
 import HomeForm from '../components/home-form';
-import { styled } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
-
-const BGContainer = styled('div')(() => ({
-  position: 'relative'
-}));
-
-const BGImage = styled('img')(() => ({
-  display: 'block',
-  minWidth: '100%',
-  maxHeight: '100vh',
-  minHeight: '100vh',
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  zIndex: -2
-}));
-
-const BGOverlay = styled('div')(() => ({
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  minWidth: '100%',
-  minHeight: '100vh',
-  zIndex: -1,
-  backgroundColor: 'rgba(0, 0, 0, 0.5)'
-}));
 
 export default function Home(props) {
   return (
     <>
-      <BGContainer>
-        <BGOverlay />
-        <BGImage src="sf2background.png" alt="street fighter 2 background"/>
-      </BGContainer>
       <Grid container justifyContent="center" alignContent="center">
         <Grid item>
           <HomeForm />
