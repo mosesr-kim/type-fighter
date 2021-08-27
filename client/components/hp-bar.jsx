bb99cf35a10945b40fcede73068a469de7c2310a
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-08-27 14:29:15 -0700

    hp bar now rotates based on side prop

--- client/components/hp-bar.jsx
+++ client/components/hp-bar.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import { Box, styled } from '@material-ui/core';
 
 export default function HPBar(props) {
-  const { hp } = props;
+  const { hp, side } = props;
+  let rotation = 0;
+  if (side === 'left') {
+    rotation = 0;
+  } else if (side === 'right') {
+    rotation = 180;
+  }
 
   const BarContainer = styled('div')({
     border: '3px solid white',
     width: '500px',
-    height: '3rem'
+    height: '3rem',
+    transform: `rotateY(${rotation}deg)`
   });
 
   const Bar = styled('div')({
     backgroundColor: 'red',
     height: '100%',
     width: `${hp}%`
-
   });
+
   return (
     <Box m={2}>
       <BarContainer>
