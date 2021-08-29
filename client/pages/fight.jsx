0bf9f3c76694c432cb86a7da0f5f59eec5f864e8
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-08-28 22:38:27 -0700

    laid groundwork for getting metaData

--- client/pages/fight.jsx
+++ client/pages/fight.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import TypingBox from '../components/typing-box';
 
+const dummyMeta = {
+  gameId: 1,
+  hostId: 1,
+  hostUsername: 'Player 1',
+  oppId: 2,
+  oppUsername: 'Player 2'
+};
+
 export default function Fight(props) {
+  const [metaData, setMetaData] = useState(null);
+
+  useEffect(() => {
+    setMetaData(dummyMeta);
+  }, []);
+
+  if (!metaData) {
+    return <></>;
+  }
+
   return (
     <TypingBox text="Type Text Here" />
   );
