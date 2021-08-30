87a9d316e19486e23ceba4f59205c2e878d8f8a3
Solomon Jin <solomonjin135@yahoo.com>
2021-08-30 00:18:54 -0700

    Created a UserContext

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from './pages/home';
 import Lobby from './pages/lobby';
 import Fight from './pages/fight';
 import { styled } from '@material-ui/core';
 import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 import RouterContext from './lib/router-context';
+import UserContext from './lib/user-context';
 
 const BGContainer = styled('div')(() => ({
   position: 'relative'
@@ -49,17 +50,19 @@ function App() {
         <BGImage src="sf2background.png" alt="street fighter 2 background" />
       </BGContainer>
 
-      <Switch>
-        <Route path="/lobby" >
-          <Lobby />
-        </Route>
-        <Route path="/fight" >
-          <Fight />
-        </Route>
-        <Route path="/" >
-          <Home />
-        </Route>
-      </Switch>
+      <UserContext.Provider>
+        <Switch>
+          <Route path="/lobby" >
+            <Lobby />
+          </Route>
+          <Route path="/fight" >
+            <Fight />
+          </Route>
+          <Route path="/" >
+            <Home />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
     </RouterContext.Provider>
   );
 }
