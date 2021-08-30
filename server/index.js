02f26a07b2ac9a5510f3d6e6a6c1eaca2d182db1
Solomon Jin <solomonjin135@yahoo.com>
2021-08-30 00:59:18 -0700

    Added a auth middleware for socket connections

--- server/index.js
+++ server/index.js
@@ -17,11 +17,24 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.use((socket, next) => {
+  cookieParser(socket.request, null, next);
+});
 
+io.use((socket, next) => {
+  const cookies = socket.request.signedCookies;
+  if (!cookies.userToken) {
+    next(new ClientError(401, 'authentication required'));
+  }
+
+  const payload = jwt.verify(cookies.userToken, process.env.TOKEN_SECRET);
+
+  socket.user = payload;
+  next();
 });
 
 io.on('connection', socket => {
   const { gameId } = socket.handshake.query;
+  console.log(socket.user);
 
   if (gameId) {
     socket.join(gameId);
