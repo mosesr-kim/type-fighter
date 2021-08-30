49c76d4a445251f159d31c314f1df6c670a90bdd
Moses Kim <mosesrandykim@gmail.com>
2021-08-30 02:41:17 -0700

    added animations to home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeForm from '../components/home-form';
 import { Grid } from '@material-ui/core';
+import Animations from '../components/animations';
 
 export default function Home(props) {
   return (
@@ -8,6 +9,7 @@ export default function Home(props) {
       <Grid container justifyContent="center" alignContent="center">
         <Grid item>
           <HomeForm />
+          <Animations />
         </Grid>
       </Grid>
     </>
