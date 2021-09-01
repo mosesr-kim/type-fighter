52ea285a4fbd6a3effe19b1f7d69c6bcb98b159e
Moses Kim <mosesrandykim@gmail.com>
2021-09-01 14:29:34 -0700

    added how to play

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -88,6 +88,35 @@ export default function Home(props) {
           </Grid>
         </Grid>
       </Grid>
+
+      <Grid item>
+        <Grid container>
+          <Grid item xs={6}>
+            <Grid container direction='column'>
+              <Grid item>
+                <h2>How to play</h2>
+              </Grid>
+              <Grid item>
+                <p>
+                  Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                  Aliquam at velit urna. Nam ex ligula, elementum eget viverra sit
+                  amet, efficitur a tortor. Integer tristique egestas euismod.
+                  Fusce sed risus ac urna mollis pellentesque. Sed et lacinia lectus.
+                  Cras dapibus lacinia ligula, sit amet ullamcorper metus sodales non.
+                  Maecenas fringilla erat ut eros consequat vestibulum. Ut porta sit
+                  amet felis eu imperdiet. Sed id leo ligula.
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container>
+              <p>Gif Here</p>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
     </Grid>
   );
 }
