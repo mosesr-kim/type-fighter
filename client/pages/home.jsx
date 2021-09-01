82022bcb6a6f936ad5cbc32bab1f74dd13f86407
Moses Kim <mosesrandykim@gmail.com>
2021-09-01 13:37:37 -0700

    added main section in grid layout for landing page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -25,21 +25,43 @@ export default function Home(props) {
         </Grid>
       </Grid>
       <Grid item>
-        <Grid item>
-          <Link to="/game">
-            <button>Play</button>
-          </Link>
-        </Grid>
+        <Link to="/game">
+          <button>Play</button>
+        </Link>
       </Grid>
 
-      {/* <Grid item>
+      <Grid item>
         <Grid container>
-
+          <Grid item xs={6}>
+            <Grid container direction='column'>
+              <Grid item>
+                <h2>Type Fighter</h2>
+              </Grid>
+              <Grid item>
+                <p>
+                  Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                  Aliquam at velit urna. Nam ex ligula, elementum eget viverra sit
+                  amet, efficitur a tortor. Integer tristique egestas euismod.
+                  Fusce sed risus ac urna mollis pellentesque. Sed et lacinia lectus.
+                  Cras dapibus lacinia ligula, sit amet ullamcorper metus sodales non.
+                  Maecenas fringilla erat ut eros consequat vestibulum. Ut porta sit
+                  amet felis eu imperdiet. Sed id leo ligula.
+                </p>
+              </Grid>
+              <Grid item>
+                <Link to="/game">
+                  <button>Play</button>
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container>
+              <p>Gif Here</p>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid container>
-
-        </Grid>
-      </Grid> */}
+      </Grid>
     </Grid>
   );
 }
