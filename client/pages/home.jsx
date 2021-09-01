4feb8c81a76d4b503a9fc4624078f383c3a7469c
Moses Kim <mosesrandykim@gmail.com>
2021-09-01 13:45:37 -0700

    added the About the game section in the landing page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -62,6 +62,32 @@ export default function Home(props) {
           </Grid>
         </Grid>
       </Grid>
+
+      <Grid item>
+        <Grid container>
+          <Grid item xs={6}>
+            <p>Some image here</p>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container direction='column'>
+              <Grid item>
+                <h2>About the game</h2>
+              </Grid>
+              <Grid item>
+                <p>
+                  Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                  Aliquam at velit urna. Nam ex ligula, elementum eget viverra sit
+                  amet, efficitur a tortor. Integer tristique egestas euismod.
+                  Fusce sed risus ac urna mollis pellentesque. Sed et lacinia lectus.
+                  Cras dapibus lacinia ligula, sit amet ullamcorper metus sodales non.
+                  Maecenas fringilla erat ut eros consequat vestibulum. Ut porta sit
+                  amet felis eu imperdiet. Sed id leo ligula.
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
