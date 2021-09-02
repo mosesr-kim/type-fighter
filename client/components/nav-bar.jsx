a1f3df3afbf7fa0594ae5de7983bff746a475d86
Solomon Jin <solomonjin135@yahoo.com>
2021-09-01 17:38:03 -0700

    Created a separate navbar component

---
+++ client/components/nav-bar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Grid, Link } from '@material-ui/core';
+
+export default function NavBar(props) {
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <Link to="/">
+          Home
+        </Link>
+      </Grid>
+      <Grid item>
+        <Link to="/game">
+          <button>About the game</button>
+        </Link>
+      </Grid>
+      <Grid item>
+        <Link to="/game">
+          <button>Meet the developers</button>
+        </Link>
+      </Grid>
+    </Grid>
+  );
+}
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavBar } from '../components';
 import { Grid } from '@material-ui/core';
 
 export default function Home(props) {
   return (
     <Grid container style={{ maxWidth: '1080px', margin: '0 auto ', justifyContent: 'space-between' }}>
       <Grid item>
-        <Grid container spacing={2}>
-          <Grid item>
-            <Link to="/game">
-              <button>Home</button>
-            </Link>
-          </Grid>
-          <Grid item>
-            <Link to="/game">
-              <button>About the game</button>
-            </Link>
-          </Grid>
-          <Grid item>
-            <Link to="/game">
-              <button>Meet the developers</button>
-            </Link>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Link to="/game">
-          <button>Play</button>
-        </Link>
+
       </Grid>
 
       <Grid item>
