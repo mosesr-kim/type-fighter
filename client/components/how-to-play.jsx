59840ee82ae13d75c13a63944135e098f9f39050
Moses Kim <mosesrandykim@gmail.com>
2021-09-01 22:55:43 -0700

    merged from master and added the MyTypography instead of Typography

--- client/components/how-to-play.jsx
+++ client/components/how-to-play.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid, styled, Typography } from '@material-ui/core';
+import { Grid, styled } from '@material-ui/core';
+import MyTypography from './my-typography';
 
 const HowToPlayContainer = styled(Grid)({
   color: 'white'
@@ -11,22 +12,22 @@ const Gif = styled('img')({
 
 export default function HowToPlay(props) {
   return (
-    <HowToPlayContainer container spacing={3}>
+    <HowToPlayContainer container spacing={3} id="about-game">
       <Grid item sm={6}>
         <Gif src="/media/creating-user.gif" alt="creating user gif" />
       </Grid>
       <Grid item sm={6}>
         <Grid container>
           <Grid container direction='column'>
-            <Typography variant="h4" style={{ fontFamily: 'retro, sans-serif', color: '#f24956' }}>How to play</Typography>
-            <Typography variant="body1" style={{ lineHeight: '2.2' }}>
+            <MyTypography variant="h4" style={{ fontFamily: 'retro, sans-serif', color: '#f24956' }}>How to play</MyTypography>
+            <MyTypography variant="body1" style={{ lineHeight: '2.2' }}>
               &#8226; Enter your username and select your character.<br/>
               &#8226; Join an existing game by clicking JOIN GAME.<br />
               &#8226; Or challenge others by clicking POST A GAME.<br/>
               &#8226; Once a game begins, press TAB to being typing.<br />
               &#8226; Whoever finishes typing the phrase first attacks the opponent.<br />
               &#8226; Last man standing wins!
-            </Typography>
+            </MyTypography>
           </Grid>
         </Grid>
       </Grid>
