ace9474c5969218556909032d2b0c3d5b45271b7
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-09-01 22:11:31 -0700

    added gif

--- client/components/about-the-game.jsx
+++ client/components/about-the-game.jsx
@@ -1,36 +1,32 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
-import { Link as RouterLink } from 'react-router-dom';
+import { Grid, styled, Typography } from '@material-ui/core';
+import PlayButton from './play-button';
+
+const AboutGameContainer = styled(Grid)({
+  color: 'white'
+});
+
+const Gif = styled('img')({
+  maxWidth: '100%'
+});
 
 export default function AboutTheGame(props) {
   return (
-    <Grid container>
+    <AboutGameContainer container>
       <Grid item sm={6}>
-        <Grid container>
-          <Grid item>
-            <Typography variant="h3">Type Fighter</Typography>
-            <Typography variant="body1">
-              Type Fighter allows you to put your typing skills to the
-              test against other players in a real-time, online
-              experience. Race to complete a phrase before your
-              opponent to deal damage and eventually defeat your
-              opposition.
-            </Typography>
-          </Grid>
-          <Grid item>
-          </Grid>
-          <Grid item>
-            <RouterLink to="/game">
-              <button>Play</button>
-            </RouterLink>
-          </Grid>
-        </Grid>
+        <Typography variant="h3" style={{ fontFamily: 'retro, sans-serif' }}>Type Fighter</Typography>
+        <Typography variant="body1">
+          Type Fighter allows you to put your typing skills to the
+          test against other players in a real-time, online
+          experience. Race to complete a phrase before your
+          opponent to deal damage and eventually defeat your
+          opposition.
+        </Typography>
+        <PlayButton />
       </Grid>
       <Grid item sm={6}>
-        <Grid container>
-          <p>Gif Here</p>
-        </Grid>
+        <Gif src="/media/about-game.gif" />
       </Grid>
-    </Grid>
+    </AboutGameContainer>
   );
 }
