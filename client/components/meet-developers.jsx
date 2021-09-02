5553f3d2d1ff5b78f7f32ddb946407310c09a34b
Moses Kim <mosesrandykim@gmail.com>
2021-09-02 02:20:25 -0700

    completed meet the devs

--- client/components/meet-developers.jsx
+++ client/components/meet-developers.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Grid, Typography, styled, Paper } from '@material-ui/core';
+import { Grid, Typography, styled, Paper, IconButton } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const developers = [
   {
     URL: '/media/joseph.jpg',
     name: 'Joseph Nguyen',
     github: 'https://github.com/josephtnguyen',
-    linkedin: 'https://www.linkedin.com/in/josephtringuyen/'
+    linkedin: 'https://www.linkedin.com/in/josephtringuyen/',
+    blurb: 'I am a software engineer with a background in mathematics and a passion for learning.'
   },
   {
     URL: '/media/moses.jpg',
     name: 'Moses Kim',
     github: 'https://github.com/mosesr-kim',
-    linkedin: 'https://www.linkedin.com/in/mosesr-kim/'
+    linkedin: 'https://www.linkedin.com/in/mosesr-kim/',
+    blurb: 'Besides programming, I love listening to electronic music, working on cars, and building custom mechanical keyboards.'
   },
   {
     URL: '/media/solomon.jpg',
     name: 'Solomon Jin',
     github: 'https://github.com/solomonjin',
-    linkedin: 'https://www.linkedin.com/in/solomon-jin/'
+    linkedin: 'https://www.linkedin.com/in/solomon-jin/',
+    blurb: 'A full-stack web developer that loves learning new languages, whether they are human or computer-based.'
   }
 ];
 
@@ -37,15 +42,29 @@ const profiles = developers.map(developer => {
           {
             display: 'flex',
             justifyContent: 'center',
-            backgroundColor: '#816a65'
+            backgroundColor: '#816a65',
+            padding: '1rem'
           }
         }>
         <Grid container justifyContent="center">
           <StyledImage src={developer.URL} />
-          <Grid item>
-            <Typography variant='h5' style={{ color: '#fefef2' }}>
+          <Grid item xs={12}>
+            <Typography variant='h5' style={{ color: '#fefef2' }} align="center">
               {developer.name}
             </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant='body1' style={{ color: '#fefef2' }} align="center">
+              {developer.blurb}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} style={{ display: 'flex' }} justifyContent="space-evenly">
+            <IconButton href={developer.github} target="_blank">
+              <GitHubIcon fontSize="large"/>
+            </IconButton>
+            <IconButton href={developer.linkedin} target="_blank">
+              <LinkedInIcon fontSize="large"/>
+            </IconButton>
           </Grid>
         </Grid>
       </Paper>
@@ -56,7 +75,7 @@ const profiles = developers.map(developer => {
 export default function MeetDevelopers(props) {
   return (
     <>
-      <Grid container>
+      <Grid container id="meet-devs">
         <Grid item xs={12}>
           <Typography variant="h4" style={{ fontFamily: 'retro, sans-serif', color: '#f24956' }}>
             Meet the Developers
