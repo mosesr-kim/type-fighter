829b1a6966ec4be858d7603f7bbcb7b09f6093b4
Moses Kim <mosesrandykim@gmail.com>
2021-09-01 22:44:03 -0700

    changed the task list for how to play

--- client/components/how-to-play.jsx
+++ client/components/how-to-play.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Grid, styled } from '@material-ui/core';
+import { Grid, styled, Typography } from '@material-ui/core';
+
+const HowToPlayContainer = styled(Grid)({
+  color: 'white'
+});
 
 const Gif = styled('img')({
   maxWidth: '100%'
@@ -7,26 +11,25 @@ const Gif = styled('img')({
 
 export default function HowToPlay(props) {
   return (
-    <Grid container>
+    <HowToPlayContainer container spacing={3}>
       <Grid item sm={6}>
         <Gif src="/media/creating-user.gif" alt="creating user gif" />
       </Grid>
       <Grid item sm={6}>
         <Grid container>
           <Grid container direction='column'>
-            <Grid item>
-              <h2>How to play</h2>
-            </Grid>
-            <Grid item>
-              <p>&#8226; Enter your username and select your character.</p>
-              <p>&#8226; Join an existing game by clicking JOIN GAME, or challenge others by clicking POST A GAME.</p>
-              <p>&#8226; Once a game begins, press TAB to being typing.</p>
-              <p>&#8226; Whoever finishes typing the phrase first wins the chance to attack their opponent.</p>
-              <p>&#8226; Last man standing wins!</p>
-            </Grid>
+            <Typography variant="h4" style={{ fontFamily: 'retro, sans-serif', color: '#f24956' }}>How to play</Typography>
+            <Typography variant="body1" style={{ lineHeight: '2.2' }}>
+              &#8226; Enter your username and select your character.<br/>
+              &#8226; Join an existing game by clicking JOIN GAME.<br />
+              &#8226; Or challenge others by clicking POST A GAME.<br/>
+              &#8226; Once a game begins, press TAB to being typing.<br />
+              &#8226; Whoever finishes typing the phrase first attacks the opponent.<br />
+              &#8226; Last man standing wins!
+            </Typography>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </HowToPlayContainer>
   );
 }
